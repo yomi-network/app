85c7cc67ef63938fdb4037e3e8f6e678e1f99d56
Tania Marin <taniafcc@gmail.com>
2017-04-28 04:20:09 -0500

    Change list's appearance
    Resume version of option list.

--- componentes/Start.js
+++ componentes/Start.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
-import { Text, TextInput, View, StyleSheet,ListView } from 'react-native';
+import { Text,
+    TextInput,
+    View,
+    StyleSheet,
+    ListView,
+    ScrollView,
+    TouchableOpacity,
+    Image} from 'react-native';
 import DrawerExample from './Header';
 import SearchBar from './SearchBar';
+import {StackNavigator} from 'react-navigation';
+import Terms from './Terminos';
+import Header from './Header';
+import * as RecipesApi from './RecipesFromApi';
+import User from './User';
+import * as Menus from './Menus';
+import ImagePicker from './ImagePicker';
+import Banner from './Banner';
 
 const opciones = [
     "Perfil",
@@ -11,7 +26,129 @@ const opciones = [
     "Terminos",
 ];
 
-export default class Start extends Component {
+const ModalStack = {
+    ListRecipe: {
+        name: "Mis recetas",
+        description: "Listado de foo baz",
+        image: require('../imagenes/heart.png'),
+        screen: RecipesApi.default.Recipes,
+        },
+    ListMenu: {
+        name: "Mis menus",
+        description: "Listado de foo baz",
+        screen: Menus.default.Menus,
+    },
+    Perfil: {
+        name: "Perfil",
+        description: "Vista de perfil",
+        image: require('../imagenes/account-circle.png'),
+        screen: User,
+    },
+    /*ShowRecipe: {
+        name: "Muestra una receta",
+        description: "Muestra el detalle de la receta",
+        path: 'ShowRecipe/:name',
+        screen: RecipesApi.default.SingleRecipe,
+        navigationOptions: {
+            title: "Receta",
+            headerMode: 'none',
+            headerStyle: {
+                backgroundColor: 'red',
+            }
+        },
+    },*/
+    NewRecipe:{
+        name: "Crea un nueva receta",
+        description: "Vista para crear una nueva receta",
+        image: require("../imagenes/plus-circle-outline.png"),
+        screen: RecipesApi.default.NewRecipe,
+    },
+    /*NewMenu:{
+        name: "Crea un nuevo menu",
+        description: "Vista para crear nuevo menu",
+        path: 'NewMenu',
+        screen: Menus.default.Newmenu,
+    },
+    ShowMenu: {
+        name: "Muestra una receta",
+        description: "Muestra el detalle de la receta",
+        path: 'Showmenu/:name',
+        screen: Menus.default.Singlemenu,
+    },*/
+};
+
+
+
+const MainScreen = ({ navigation }) => (
+  <View style={{backgroundColor: "#fff"},styles.barra}>
+    <Banner />
+    {Object.keys(ModalStack).map((routeName: string) =>
+      <TouchableOpacity
+        key={routeName}
+        onPress={() => {
+          const { path, params, screen } = ModalStack[routeName];
+          const { router } = screen;
+          const action = path && router.getActionForPathAndParams(path, params);
+          navigation.navigate(routeName, {}, action);
+        }}
+      >
+        <View style={styles.item}>
+          <Image style={styles.image} source = {ModalStack[routeName].image} />
+        </View>
+      </TouchableOpacity>
+    )}
+  </View>
+);
+
+const Start = StackNavigator({
+    ...ModalStack,
+    Index: {
+        screen: MainScreen,
+    },
+},
+{
+    initialRouteName: 'Index',
+    headerMode: 'none',
+    mode: 'card',
+});
+
+export default Start;
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: '#fff',
+    paddingHorizontal: 4,
+    paddingVertical: 3,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: '#ddd',
+  },
+  image: {
+    width: 36,
+    height: 36,
+    alignSelf: 'center',
+    marginBottom: 20,
+    resizeMode: 'contain',
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#444',
+  },
+  description: {
+    fontSize: 13,
+    color: '#999',
+  },
+  barra: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      backgroundColor: "#ccc",
+      width: -1,
+      marginTop: 10,
+  }
+});
+
+
+/*export default class Start extends Component {
     constructor(props) {
     super(props);
     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
@@ -24,7 +161,7 @@ export default class Start extends Component {
         <ListView
             style={estilosStart.item}
           dataSource={this.state.dataSource}
-          renderRow={(rowData) => <Text style={estilosStart.fondo}>{rowData}</Text>}
+          renderRow={(rowData) => <Text onPress={()=>{}} style={estilosStart.fondo}>{rowData}</Text>}
         />
       </View>
         );
@@ -47,6 +184,6 @@ var estilosStart = StyleSheet.create({
         borderBottomColor: '#ddd',
     }
 
-});
+});*/
 
 //export default Start;
