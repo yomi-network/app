9e8bb2a7345091fc4013043bb710961ada936c5b
Tania Marin <taniafcc@gmail.com>
2017-04-28 08:44:15 -0500

    Add MenuBar component

---
+++ componentes/MenuBar.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import { Text,
+    TextInput,
+    View,
+    StyleSheet,
+    ListView,
+    ScrollView,
+    TouchableOpacity,
+    Image} from 'react-native';
+import DrawerExample from './Header';
+import SearchBar from './SearchBar';
+import {StackNavigator} from 'react-navigation';
+import Terms from './Terminos';
+import Header from './Header';
+import * as RecipesApi from './RecipesFromApi';
+import User from './User';
+import * as Menus from './Menus';
+import ImagePicker from './ImagePicker';
+import Banner from './Banner';
+import TimeLine from './TimeLine';
+
+class MenuBar extends Component {
+    constructor (props){
+        super(props)
+    }
+    render(){
+        return(
+            <View style={styles.barra}>
+                <TouchableOpacity>
+                    <Image style={styles.image}
+                    source={require('../imagenes/heart.png')}/>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Image style={styles.image}
+                    source={require('../imagenes/plus-circle-outline.png')}/>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Image style={styles.image}
+                    source={require('../imagenes/account-circle.png')} />
+                </TouchableOpacity>
+            </View>
+        )
+    }
+}
+
+export default MenuBar;
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: '#fff',
+    paddingHorizontal: 4,
+    paddingVertical: 3,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: '#ddd',
+  },
+  image: {
+    width: 36,
+    height: 36,
+    alignSelf: 'center',
+    marginTop: 5,
+    marginBottom: 5,
+    resizeMode: 'contain',
+  },
+  barra: {
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      alignItems: 'center',
+      backgroundColor: "#ccc",
+      width: -1,
+      marginTop: 10,
+  }
+});
